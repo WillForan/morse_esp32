4306f4b449c30e8bddecd57f0cf9a2c18a3dbf20
Will Foran <wforan1@andrew.cmu.edu>
2019-01-27 16:01:59 -0500

    ugly morse<->alpha

--- morse.ino
+++ morse.ino
@@ -3,6 +3,10 @@ int ditButton = 12; //D12
 int dahButton = 25; //D25
 int funcButton = 27; //D27
 
+// initialize function
+char morse_to_char(String keySeq);
+
+
 // push type has a count and button down time
 struct push_t { 
   push_t(): cnt(0), down(0), last(0) {};
@@ -54,14 +58,14 @@ struct state_t {
 void reset_state(state_t &s){
     s.prevSent = s.keySeq;
     s.keySeq   = "";
-    s.lookup   = 2^7;
     s.lastPush = 0;
     s.n_dd     = 0;
 }
 void send_state(state_t &s){
     if(!s.keySeq.equals("") ) {
-       Serial.printf("send ind %d (len %d) == %s\n", 
-             s.lookup, s.n_dd, s.keySeq.c_str()); 
+       char key = morse_to_char(s.keySeq);
+       Serial.printf("send %c (len %d) == %s\n", 
+             key, s.n_dd, s.keySeq.c_str()); 
     }
 }
 /*
@@ -71,7 +75,6 @@ void update_state(state_t &s, bool dah, unsigned long t){
       char add;
       if(dah) {
          add = '_';
-         s.lookup |= s.lookup << s.n_dd;// _ is 1
       } else {
          add = '.';
       }
@@ -129,4 +132,68 @@ void loop() {
     delay(20);
     digitalWrite(ledPin, LOW);
 
+}
+
+
+
+
+char morse_to_char(String keySeq){
+   // not actually morse code
+   if(keySeq.equals("._._"))    {return('\n');}
+
+   else if(keySeq.equals("._"))      {return('a');}
+   else if(keySeq.equals("_..."))    {return('b');}
+   else if(keySeq.equals("_._."))    {return('c');}
+   else if(keySeq.equals("_.."))     {return('d');}
+   else if(keySeq.equals("."))       {return('e');}
+   else if(keySeq.equals(".._."))    {return('f');}
+   else if(keySeq.equals("__."))     {return('g');}
+   else if(keySeq.equals("...."))    {return('h');}
+   else if(keySeq.equals(".."))      {return('i');}
+   else if(keySeq.equals(".___"))    {return('j');}
+   else if(keySeq.equals("_._"))     {return('k');}
+   else if(keySeq.equals("._.."))    {return('l');}
+   else if(keySeq.equals("__"))      {return('m');}
+   else if(keySeq.equals("_."))      {return('n');}
+   else if(keySeq.equals("___"))     {return('o');}
+   else if(keySeq.equals(".__."))    {return('p');}
+   else if(keySeq.equals("__._"))    {return('q');}
+   else if(keySeq.equals("._."))     {return('r');}
+   else if(keySeq.equals("..."))     {return('s');}
+   else if(keySeq.equals("_"))       {return('t');}
+   else if(keySeq.equals(".._"))     {return('u');}
+   else if(keySeq.equals("..._"))    {return('v');}
+   else if(keySeq.equals(".__"))     {return('w');}
+   else if(keySeq.equals("_.._"))    {return('x');}
+   else if(keySeq.equals("_.__"))    {return('y');}
+   else if(keySeq.equals("__.."))    {return('z');}
+   else if(keySeq.equals(".____"))   {return('1');}
+   else if(keySeq.equals("..___"))   {return('2');}
+   else if(keySeq.equals("...__"))   {return('3');}
+   else if(keySeq.equals("...._"))   {return('4');}
+   else if(keySeq.equals("....."))   {return('5');}
+   else if(keySeq.equals("_...."))   {return('6');}
+   else if(keySeq.equals("__..."))   {return('7');}
+   else if(keySeq.equals("___.."))   {return('8');}
+   else if(keySeq.equals("____."))   {return('9');}
+   else if(keySeq.equals("_____"))   {return('0');}
+   else if(keySeq.equals("._._._"))  {return('.');}
+   else if(keySeq.equals("__..__"))  {return(',');}
+   else if(keySeq.equals("..__.."))  {return('?');}
+   else if(keySeq.equals(".____."))  {return('\'');}
+   else if(keySeq.equals("_._.__"))  {return('!');}
+   else if(keySeq.equals("_.._."))   {return('/');}
+   else if(keySeq.equals("_.__."))   {return('(');}
+   else if(keySeq.equals("_.__._"))  {return(')');}
+   else if(keySeq.equals("._..."))   {return('&');}
+   else if(keySeq.equals("___..."))  {return(':');}
+   else if(keySeq.equals("_._._."))  {return(';');}
+   else if(keySeq.equals("_..._"))   {return('=');}
+   else if(keySeq.equals("._._."))   {return('+');}
+   else if(keySeq.equals("_...._"))  {return('-');}
+   else if(keySeq.equals("..__._"))  {return('_');}
+   else if(keySeq.equals("._.._."))  {return('"');}
+   else if(keySeq.equals("..._.._")) {return('$');}
+   else if(keySeq.equals(".__._."))  {return('@');}
+   else {return(' ');}
 }
